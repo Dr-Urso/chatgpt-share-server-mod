bc3a1392f771f12cd447a8b98cfaf784a1a2e9aa
DrKuma <scy09105@gmail.com>
2024-01-26 21:35:04 +0800

    转换

--- release.sh
+++ release.sh
@@ -12,11 +12,11 @@ fi
 
 cd backend
 gf build main.go -a amd64 -s linux -p ./temp
-gf docker main.go -p -t xyhelper/chatgpt-share-server:latest
+gf docker main.go -p -t drkuma/chatgpt-share-server:latest
 now=$(date +"%Y%m%d%H%M%S")
 # 以当前时间为版本号
-docker tag xyhelper/chatgpt-share-server:latest xyhelper/chatgpt-share-server:$now
-docker push xyhelper/chatgpt-share-server:$now
+docker tag drkuma/chatgpt-share-server:latest drkuma/chatgpt-share-server:$now
+docker push drkuma/chatgpt-share-server:$now
 echo "release success" $now
 # 写入发布日志 release.log
 echo $now >> release.log
